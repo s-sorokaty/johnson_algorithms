5039c4d6273903e3147702e0350f1ff12b177e4c
s-sorokaty <sorokaty0824@gmail.ru>
2023-03-09 12:20:35 +0700

    fix

--- methods.py
+++ methods.py
@@ -6,6 +6,7 @@ class METHOD_ABSTRACT():
     def read_data(self, filename) -> pd.DataFrame: 
         data = pd.read_csv(filename)
         return data
+    
 # ПЕРВОЕ ОБОБЩЕНИЕ ДЖОНСОНА - СНАЧАЛА ИДУТ ДЕТАЛИ С МИНИМАЛЬНЫЙМ ВРЕМЕНЕМ ОБРАБОТКИ НА ПЕРВОМ СТАНКЕ
 class FirstJohnsonGeneralizations(METHOD_ABSTRACT):
     def create_count_window(self, filename): 
@@ -18,11 +19,10 @@ class FirstJohnsonGeneralizations(METHOD_ABSTRACT):
             dpg.add_text(first_row.to_string())
 
 # ВТОРОЕ ОБОБЩЕНИЕ ДЖОНСОНА - СНАЧАЛА ИДУТ ДЕТАЛИ С МАКСИМАЛЬНЫМ ВРЕМЕНЕМ ОБРАБОТКИ НА ПОСЛЕДНЕМ СТАНКЕ
-
 class SecondJohnsonGeneralizations(METHOD_ABSTRACT):
     def create_count_window(self, filename): 
         data = super().read_data(filename)
-        last_row = data.loc[data.index[-1]].sort_values(ascending=False)
+        last_row = data.loc[data.index[-1]][1::].sort_values(ascending=False)
         with dpg.window(label=f"Второе обощение Джонсона {filename}"):
             dpg.add_text("ВХОДНОЙ МАССИВ ДЕТАЛЕЙ")
             dpg.add_text(data.to_string())
