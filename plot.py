a3afc46bdae53c92e5350c9960b63f13dd562f37
s-sorokaty <sorokaty0824@gmail.com>
2023-03-15 12:46:44 +0700

    color generation

--- plot.py
+++ plot.py
@@ -4,6 +4,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import datetime as dt
 
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
 def plot(df:pd.DataFrame, order:pd.Series):
     #plt.style.use('dark_background')
     fig, gnt = plt.subplots()
@@ -22,7 +27,7 @@ def plot(df:pd.DataFrame, order:pd.Series):
     delays =  [0 for i in range(1, len(y_labels)+1)]
     gnt.grid(True)
     colors = ['red', 'blue', 'black', 'green', 'orange']
-
+    cmap = get_cmap(10)
     detail = 0
     for index in order.index:
         data = df[index]
@@ -31,21 +36,17 @@ def plot(df:pd.DataFrame, order:pd.Series):
 
         for val in data:
             if detail == 0:
-                gnt.broken_barh([(next_delay, val)], (y_ticks[workspace], 2), facecolors=colors[detail], label=data.name)
+                gnt.broken_barh([(next_delay, val)], (y_ticks[workspace], 2), facecolors=cmap(detail), label=data.name)
                 next_delay += val
                 delays[workspace] += next_delay
-                
             else:
                 if workspace > 1 and delays[workspace] - delays[workspace-1]<0:
                     
-                    gnt.broken_barh([(-(delays[workspace] - delays[workspace-1])+delays[workspace], val)], (y_ticks[workspace], 2), facecolors=colors[detail], label=data.name)
+                    gnt.broken_barh([(-(delays[workspace] - delays[workspace-1])+delays[workspace], val)], (y_ticks[workspace], 2), facecolors=cmap(detail), label=data.name)
                     delays[workspace] += -(delays[workspace] - delays[workspace-1])+val
                 else:
-                    gnt.broken_barh([(delays[workspace], val)], (y_ticks[workspace], 2), facecolors=colors[detail], label=data.name)
+                    gnt.broken_barh([(delays[workspace], val)], (y_ticks[workspace], 2), facecolors=cmap(detail), label=data.name)
                     delays[workspace] += val
-
-                
-
 
             workspace += 1
         detail +=1
