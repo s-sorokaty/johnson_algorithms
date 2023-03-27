ca259e77a4b7d460d574b11ab5a3a45331135d38
s-sorokaty <sorokaty0824@gmail.com>
2023-03-27 13:12:09 +0700

    update filesave

--- methods.py
+++ methods.py
@@ -8,10 +8,12 @@ class METHOD_ABSTRACT():
     def read_data(self, filename:str) -> pd.DataFrame: 
         data = pd.read_csv(filename)
         return data
-    
+        
 
 # ПЕРВОЕ ОБОБЩЕНИЕ ДЖОНСОНА - СНАЧАЛА ИДУТ ДЕТАЛИ С МИНИМАЛЬНЫЙМ ВРЕМЕНЕМ ОБРАБОТКИ НА ПЕРВОМ СТАНКЕ
 class FirstJohnsonGeneralizations(METHOD_ABSTRACT):
+    completed_details:int = 0
+
     def create_count_window(self, filename): 
         data = super().read_data(filename)
         first_row = data.loc[data.index[0]][1::].sort_values()
@@ -23,7 +25,17 @@ class FirstJohnsonGeneralizations(METHOD_ABSTRACT):
             dpg.add_text("\n")
             dpg.add_text("\nДИАГРАММА ГАНТА")
             plot(data, first_row)
-            data.iloc[:, [0, -1,-2]].to_csv('result.csv', index=False)
+            def set_completed_details(value):
+                self.completed_details = value
+            def prepare_to_save():
+               details_name = first_row.index.to_list()[self.completed_details: len(first_row.index.to_list())]
+               details_name.insert(0,data.columns.to_list()[0])
+               print(details_name)
+               data[details_name].to_csv('result.csv', index=False)
+               
+            dpg.add_input_int(label="Законченные детали", callback=lambda item, value:set_completed_details(value) )
+            dpg.add_button(label="Сохранить", callback=lambda:prepare_to_save())
+
             width, height, channels, first_image = dpg.load_image("gantt1.png")
             texture_name = str(uuid.uuid4())
             with dpg.texture_registry():
@@ -35,6 +47,8 @@ class FirstJohnsonGeneralizations(METHOD_ABSTRACT):
 
 # ВТОРОЕ ОБОБЩЕНИЕ ДЖОНСОНА - СНАЧАЛА ИДУТ ДЕТАЛИ С МАКСИМАЛЬНЫМ ВРЕМЕНЕМ ОБРАБОТКИ НА ПОСЛЕДНЕМ СТАНКЕ
 class SecondJohnsonGeneralizations(METHOD_ABSTRACT):
+    completed_details:int = 0
+    
     def create_count_window(self, filename): 
         data = super().read_data(filename)
         last_row = data.loc[data.index[-1]][1::].sort_values(ascending=False)
@@ -43,9 +57,19 @@ class SecondJohnsonGeneralizations(METHOD_ABSTRACT):
             dpg.add_text(data.to_string())
             dpg.add_text("\nПОРЯДОК ЗАПУСКА ДЕТАЛЕЙ")
             dpg.add_text(last_row.to_string())
-            dpg.add_combo(label="Обработанные детали", items = last_row.index.to_list())
             plot(data, last_row)
-            data.iloc[:, [0, -1,-2]].to_csv('result.csv', index=False)
+
+            def set_completed_details(value):
+                self.completed_details = value
+            def prepare_to_save():
+               details_name = last_row.index.to_list()[self.completed_details: len(last_row.index.to_list())]
+               details_name.insert(0,data.columns.to_list()[0])
+               print(details_name)
+               data[details_name].to_csv('result.csv', index=False)
+               
+            dpg.add_input_int(label="Законченные детали", callback=lambda item, value:set_completed_details(value) )
+            dpg.add_button(label="Сохранить", callback=lambda:prepare_to_save())
+
             width, height, channels, first_image = dpg.load_image("gantt1.png")
             texture_name = str(uuid.uuid4())
             with dpg.texture_registry():
